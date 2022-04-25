03a05fb8f1be6cc8317336c7d8930e010ed4a69e
YILMER MORENO SALAS <yilmer140453@gmail.com>
2022-04-24 23:15:03 -0500

    Countries CRUD

---
+++ TiendaOnline/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TiendaOnline.Models;
+
+namespace TiendaOnline.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        //Se crea constructor
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        
+        }
+        public DbSet<Country> countries { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder); modelBuilder.Entity<Country>().HasIndex(t => t.Name).IsUnique();
+        }
+
+    }
+}
